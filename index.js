6ca605b1f2919359d3c99c67697558dd3e39b816
Jacquan Brooks <jacquanb5@gmail.com>
2021-12-28 15:05:00 -0600

    Updated work

---
+++ index.js
@@ -0,0 +1,26 @@
+let add = 1 + 80;
+let subtract = 20 - 5;
+let multiply = 5 * 9;
+let divide = 60 / 2;
+
+add = function (a, b) {
+    let add = a + b;
+    return add;
+}
+
+subtract = function (a, b) {
+    let subtract = a - b;
+    return subtract;
+}
+
+multiply = function (a, b) {
+    let multiply = a * b;
+    return multiply;
+}
+
+divide = function (a, b) {
+    let divide = a / b;
+    return divide;
+}
+let increment = n;
+increment++;
